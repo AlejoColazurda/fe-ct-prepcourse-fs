89f2ce57d08b860c8b57fbd8cd664affe4a6f4bd
Alejo <acolazurda@gmail.com>
2024-08-11 23:30:29 -0300

    Funciones con JavaScript

---
+++ C5 Fundamentos Javascript/ejercicios/0funciones.js
@@ -0,0 +1,45 @@
+// function sumaTres(x){
+//     console.log(x + 3);
+// }
+
+// sumaTres(5); // Imprime: 8
+
+
+// //PRIMERO
+// function sumaTres(x) {
+//     return x + 3;
+// }
+
+// //SEGUNDO
+// var sumaTres = function(x) {
+//     return x + 3;
+// }
+
+// //TERCERO
+// var sumaTres = (x)=>{
+//     return x + 3;
+// }
+
+// function cuidadoConElConsoleLog(nombre){
+//     console.log(nombre);
+//     return nombre;
+// }
+
+//CÓDIGO CONDICIONAL
+function viajar (destino){
+    if (destino === "Brasil"){
+        console.log("Gire a la IZQUIERDA");
+    }else if (destino === "Argentina"){
+        console.log("Gire a la DERECHA");
+    }else{
+        console.log("No se puede ir a " + destino);
+    }
+}
+
+viajar("Paraguay");
+
+
+
+
+
+
